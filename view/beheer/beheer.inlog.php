2167b0c2242f2e668afa3e40f40a156d4ddd6deb
selhorst <universitygamingnl@gmail.com>
2013-06-13 10:50:36 +0200

    Log in

--- view/beheer/beheer.inlog.php
+++ view/beheer/beheer.inlog.php
@@ -1,3 +1,5 @@
+
+
 <div class="article">
     <p class="title-big">
         Beheer systeem
@@ -6,6 +8,23 @@
 
 <div class="page-title">
     <p>
-         
+         <?php
+			if(isset($_SESSION['restriction'])){
+			
+			}else{
+		?> <form action="<?=defaults("BASE_SHORT")?>beheer/inlog/" method="SESSION">
+				<table>
+					<tr>
+						<td>Gebruikersnaam</td>
+						<td><input type="txt" size="10" name="username">
+					</tr>
+					<tr>
+						<td>Wachtwoord</td>
+						<td><input type="password" size="10" name="password">
+					</tr>
+				</table>
+			</form>
+			}
+		 ?>
     <p>
 </div>
