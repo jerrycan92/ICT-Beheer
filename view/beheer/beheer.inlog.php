50639cdd85115983627f90cfd7cda5aa3600185e
Jeffrey Zant <jeffrey_web@live.nl>
2013-06-13 12:49:19 +0200

    Pruts aanpassingen 2

--- view/beheer/beheer.inlog.php
+++ view/beheer/beheer.inlog.php
@@ -7,33 +7,35 @@
 
 <div class="page-title">
     <p>
-         <?php
-		if(isset($_SESSION['restriction'])){
-			echo " Welkom " . $ . " bent ingelogd, u kunt nu bovenaan nieuwe menu's selecteren."
-		}else{
-			if(isset($_POST["password"])){
-				if(md5($_POST["password"]) == 1)//SQL SHIT{
-						$_SESSION["restriction"] = 1
-					}
-			}else{
-			?> <form action="<?=defaults("BASE_SHORT")?>beheer/inlog/" method="POST">
-				<table>
-					<tr>
-						<td>Gebruikersnaam</td>
-						<td><input type="txt" size="10" name="username" />
-					</tr>
-					<tr>
-						<td>Wachtwoord</td>
-						<td><input type="password" size="10" name="password" />
-					</tr>
-					<tr>
-						<td></td>
-						<td><input type="button" name="button" />Log in</td>
-					</tr>
-				</table>
-			</form>
-			<?
+        <?
+		if (isset($_SESSION['restriction'])) {
+			echo "Welkom bent ingelogd, u kunt nu bovenaan nieuwe menu's selecteren.";
+		} else {
+			if (isset($_POST["password"])) {
+				if(md5($_POST["password"]) == 1) {
+					$_SESSION["restriction"] = 1;
+				}
+			} else {
+				?> 
+                <form action="<?=defaults("BASE_SHORT")?>beheer/inlog/" method="POST">
+                    <table>
+                        <tr>
+                            <td>Gebruikersnaam</td>
+                            <td><input type="text" size="10" name="username" />
+                        </tr>
+                        <tr>
+                            <td>Wachtwoord</td>
+                            <td><input type="password" size="10" name="password" />
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td><input type="submit" name="button" value="Log in" /></td>
+                        </tr>
+                    </table>
+                </form>
+                <?
 			}
-		 ?>
+		}
+		?>
     <p>
 </div>
