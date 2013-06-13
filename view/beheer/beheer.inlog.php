00d172cdab8f365070e768ea622ad074f6106280
selhorst <universitygamingnl@gmail.com>
2013-06-13 12:41:44 +0200

    Beheer inlog aanpassing

--- view/beheer/beheer.inlog.php
+++ view/beheer/beheer.inlog.php
@@ -1,5 +1,4 @@
 
-
 <div class="article">
     <p class="title-big">
         Beheer systeem
@@ -8,25 +7,33 @@
 
 <div class="page-title">
     <p>
-    	<?
-		if (isset($_SESSION['restriction'])) {
-			echo "Welkom bent ingelogd, u kunt nu bovenaan nieuwe menu's selecteren.";
-		} else {
-			?> 
-			<form action="<?=defaults("BASE_SHORT")?>beheer/inlog/" method="POST">
+         <?php
+		if(isset($_SESSION['restriction'])){
+			echo " Welkom " . $ . " bent ingelogd, u kunt nu bovenaan nieuwe menu's selecteren."
+		}else{
+			if(isset($_POST["password"])){
+				if(md5($_POST["password"]) == 1)//SQL SHIT{
+						$_SESSION["restriction"] = 1
+					}
+			}else{
+			?> <form action="<?=defaults("BASE_SHORT")?>beheer/inlog/" method="POST">
 				<table>
 					<tr>
 						<td>Gebruikersnaam</td>
-						<td><input type="txt" size="10" name="username">
+						<td><input type="txt" size="10" name="username" />
 					</tr>
 					<tr>
 						<td>Wachtwoord</td>
-						<td><input type="password" size="10" name="password">
+						<td><input type="password" size="10" name="password" />
+					</tr>
+					<tr>
+						<td></td>
+						<td><input type="button" name="button" />Log in</td>
 					</tr>
 				</table>
 			</form>
-       		<?
-		}
-		?>
+			<?
+			}
+		 ?>
     <p>
 </div>
