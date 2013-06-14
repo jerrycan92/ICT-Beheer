902ce484d009704f1450eba6ff2decb7423c0493
Jeffrey Zant <jeffrey_web@live.nl>
2013-06-14 10:49:26 +0200

    Incident management opmaak

--- view/incidentmanagement/incidentmanagement.view.php
+++ view/incidentmanagement/incidentmanagement.view.php
@@ -9,26 +9,34 @@ restrictionCheckStop();
     <div class="content">
 		<?
         $data['incident'] = $model->incidentmanagement->getIncidents();
-		?><table>
+		?>
+        <table>
 			<tr>
-				<td>ID</td>
-				<td>Identificatiecode</td>
-				<td>Datum gemeld</td>
-				<td>Datum Oplossing</td>
-				<td>* NULL betekent nog niet opgelost</td>
+				<th style="width: 40px; text-align: left;">ID</th>
+				<th style="width: 200px; text-align: left;">Identificatiecode</th>
+				<th style="width: 200px; text-align: left;">Datum gemeld</th>
+				<th style="width: 200px; text-align: left;">Datum Oplossing</th>
 			</tr>
-		<?
-        while ($show['incident'] = mysql_fetch_assoc($data['incident'])) {
-        ?> 
-				<tr>
-					<td><?$show['incident']['ID_I'];?></td>
-					<td><?$show['incident']['TXT_I_HC_Identificatiecode'];?></td> 
-					<td><?$show['incident']['DAT_I_gemeld'];?> </td>
-					<td><?$show['incident']['DAT_I_Oplossing'];?></td>
-				</tr>
-		<?
-        }
-        ?>
+			<?
+            while ($show['incident'] = mysql_fetch_assoc($data['incident'])) {
+            ?> 
+                <tr style="font-weight: normal;">
+                    <td><?=$show['incident']['ID_I'];?></td>
+                    <td><?=$show['incident']['TXT_I_HC_Identificatiecode'];?></td> 
+                    <td><?=$show['incident']['DAT_I_gemeld'];?> </td>
+                    <td>
+						<?
+                        if (!$show['incident']['DAT_I_Oplossing']) {
+							?>Nog niet opgelost<?
+						} else {
+							echo $show['incident']['DAT_I_Oplossing'];	
+						}
+						?>    
+                    </td>
+                </tr>
+            <?
+            }
+            ?>
 		</table>
     </div>
 </div>
