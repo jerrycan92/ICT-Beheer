0010872bf39c8eee0d3a42ee53418833b806f415
selhorst <universitygamingnl@gmail.com>
2013-06-14 10:23:45 +0200

    Incident opschoning

--- view/incidentmanagement/incidentmanagement.view.php
+++ view/incidentmanagement/incidentmanagement.view.php
@@ -9,9 +9,26 @@ restrictionCheckStop();
     <div class="content">
 		<?
         $data['incident'] = $model->incidentmanagement->getIncidents();
+		?><table>
+			<tr>
+				<td>ID</td>
+				<td>Identificatiecode</td>
+				<td>Datum gemeld</td>
+				<td>Datum Oplossing</td>
+				<td>* NULL betekent nog niet opgelost</td>
+			</tr>
+		<?
         while ($show['incident'] = mysql_fetch_assoc($data['incident'])) {
-            echo $show['incident']['ID_I'] . "&nbsp;" . $show['incident']['TXT_I_HC_Identificatiecode'] . "<br />";
+        ?> 
+				<tr>
+					<td><?$show['incident']['ID_I'];?></td>
+					<td><?$show['incident']['TXT_I_HC_Identificatiecode'];?></td> 
+					<td><?$show['incident']['DAT_I_gemeld'];?> </td>
+					<td><?$show['incident']['DAT_I_Oplossing'];?></td>
+				</tr>
+		<?
         }
         ?>
+		</table>
     </div>
 </div>
